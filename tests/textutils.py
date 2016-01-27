06d077d74aa9f925614018acffd9aa9bf7760a14
Tim Legrand <timlegrand.perso@gmail.com>
2016-01-27 21:23:10 +0100

    tests: complete textutils unittests

--- tests/textutils.py
+++ tests/textutils.py
@@ -11,7 +11,6 @@ def teardown():
 
 
 def test_shorten():
-
     string = 'Hi, this is a long string that should be shorten.'
     new_string, num_raw_bytes = textutils.shorten(string, size=18)
 
@@ -21,7 +20,6 @@ def test_shorten():
 
 
 def test_blocks():
-
     text = '''@@ -118,6 +118,7 @@ def git_submodules():
 
  def git_tags():
@@ -45,3 +43,101 @@ str(error))
     blocks = textutils.blocks(text, lambda x: x and x.startswith('@@'))
 
     assert len(list(blocks)) == 2
+
+
+def test_remove_superfluous_alineas():
+    text = '''
+             error = 0
+     else:
+         data = data[4:]
+-    # import cursutils
+-    # cursutils.debug()
+     if error:
+         raise Exception('Error executing "' + cmd + '" (error = ' + \
+str(error))'''.split('\n')
+
+    expected_text = '''
+         error = 0
+ else:
+     data = data[4:]
+-# import cursutils
+-# cursutils.debug()
+ if error:
+     raise Exception('Error executing "' + cmd + '" (error = ' + \
+str(error))'''.split('\n')
+
+    assert textutils.remove_superfluous_alineas(text) == expected_text
+
+
+def test_tabs_to_spaces():
+    text = '''
+\t\t\t error = 0
+\t else:
+\t\t data = data[4:]
+-    # import cursutils
+-    # cursutils.debug()
+\t if error:
+\t\t raise Exception('Error executing "' + cmd + '" (error = ' + \
+str(error))'''
+
+    expected_text = '''
+             error = 0
+     else:
+         data = data[4:]
+-    # import cursutils
+-    # cursutils.debug()
+     if error:
+         raise Exception('Error executing "' + cmd + '" (error = ' + \
+str(error))'''
+
+    assert textutils.tabs_to_spaces(text, num_spaces=4) == expected_text
+
+
+def test_lstrip_hunk():
+    text = '''
+             error = 0
+     else:
+         data = data[4:]
+-    # import cursutils
+-    # cursutils.debug()
+     if error:
+         raise Exception('Error executing "' + cmd + '" (error = ' + \
+str(error))'''.split('\n')
+
+    expected_text = '''
+         error = 0
+ else:
+     data = data[4:]
+-# import cursutils
+-# cursutils.debug()
+ if error:
+     raise Exception('Error executing "' + cmd + '" (error = ' + \
+str(error))'''.split('\n')
+
+    assert textutils.lstrip_hunk(text, offset=4) == expected_text
+
+
+def test_get_new_minimum_alinea():
+    text_a = '+    a'
+    text_b = '-b'
+    text_c = '   c'
+
+    assert textutils.get_new_minimum_alinea(
+        text_a,
+        previous_alinea=5,
+        num_ignored_chars=1) == 4
+
+    assert textutils.get_new_minimum_alinea(
+        text_a,
+        previous_alinea=2,
+        num_ignored_chars=1) == 2
+
+    assert textutils.get_new_minimum_alinea(
+        text_b,
+        previous_alinea=1,
+        num_ignored_chars=1) == 0
+
+    assert textutils.get_new_minimum_alinea(
+        text_b,
+        previous_alinea=2,
+        num_ignored_chars=0) == 0
