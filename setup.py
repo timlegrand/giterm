3e7c7f6472a6e9dfd9cd2f7c08b0ae4cf30c3d1d
Tim Legrand <timlegrand.perso+dev@gmail.com>
2017-03-19 00:03:35 +0100

    build: update list classifiers with python3 support

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Natural Language :: English',
         'Topic :: Software Development :: Version Control',
         ],
